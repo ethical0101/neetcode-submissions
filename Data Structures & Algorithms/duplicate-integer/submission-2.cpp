0ae39751649e1783d0c2d1e1f1e1e40c8cb34b12
Druthendra Nadh Chowdary <123973965+ethical0101@users.noreply.github.com>
2026-07-06 20:52:50 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> visited;
+        for(int x : nums){
+            if(visited.count(x)){
+                return true;
+            }
+
+            visited.insert(x);
+        }
+    return false;
+    }
+};
