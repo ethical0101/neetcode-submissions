b1e52ade1d3a7de2a195df22ae68a5a7e61e7937
Druthendra Nadh Chowdary <123973965+ethical0101@users.noreply.github.com>
2026-07-06 21:05:11 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char, int> mapone;
+        map<char, int> maptwo;
+
+        if(s.length() < t.length() || s.length() > t.length()){
+            return false;
+        }
+
+        for(int i = 0; i < s.length() ; i++){
+            mapone[s[i]]++;
+        }
+        for(int i = 0; i < t.length() ; i++){
+            maptwo[t[i]]++;
+        }
+
+        if(mapone != maptwo){
+            return false;
+        }
+       
+        return true;
+    }
+};
