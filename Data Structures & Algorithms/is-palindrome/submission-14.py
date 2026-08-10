298594cc4002f84b9d98a4d6085448a7e6aecfa5
Druthendra Nadh Chowdary <123973965+ethical0101@users.noreply.github.com>
2026-08-10 18:42:40 +0530

    Add: is-palindrome - submission-14

---
+++ Data Structures & Algorithms/is-palindrome/submission-14.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1;
+        s = s.lower()
+
+        while i < j:
+            if not self.an(s[i]):
+                i+=1
+            elif not self.an(s[j]):
+                j-=1
+            elif s[i]!=s[j]:
+                return False
+            else:
+                i += 1
+                j -= 1
+
+        return True
+
+    def an(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
+                
